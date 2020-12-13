e43b51a8f6ce6bd74c28d33e773479ca81bdc0dc
Celil Uysal <celiluysl15@gmail.com>
2020-12-13 03:20:04 +0300

    runtime permission

--- app/src/main/java/com/example/pintiapp/Fragments/ScanBarcodePageFragment.kt
+++ app/src/main/java/com/example/pintiapp/Fragments/ScanBarcodePageFragment.kt
@@ -1,18 +1,32 @@
 package com.example.pintiapp.Fragments
 
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import android.widget.Toolbar
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.pintiapp.BarcodeScanActivity
 import com.example.pintiapp.ViewModels.AddProductPageViewModel
 import com.example.pintiapp.R
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
 
 class ScanBarcodePageFragment : Fragment() {
 
@@ -22,6 +36,7 @@ class ScanBarcodePageFragment : Fragment() {
 
     private lateinit var viewModel: AddProductPageViewModel
     private lateinit var cardViewScanBarcode: CardView
+    val CAMERA_RQ = 101
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,16 +48,43 @@ class ScanBarcodePageFragment : Fragment() {
 
         cardViewScanBarcode = rootView.findViewById(R.id.cardViewScanBarcode)
         cardViewScanBarcode.setOnClickListener {
-            activity?.let{
-                val intent = Intent (it, BarcodeScanActivity::class.java)
-                it.startActivity(intent)
-//                it.finish()
+            checkForPermissions(android.Manifest.permission.CAMERA, "camera", CAMERA_RQ)
+        }
+        return rootView
+    }
+
+
+    private fun checkForPermissions(permission: String, name: String, requestCode: Int){
+            when {
+                activity?.let { ContextCompat.checkSelfPermission(it.applicationContext ,permission) } == PackageManager.PERMISSION_GRANTED -> {
+//                    Toast.makeText(context, "$name permission granted", Toast.LENGTH_SHORT).show()
+                    activity?.let{
+                        val intent = Intent (it, BarcodeScanActivity::class.java)
+                        it.startActivity(intent)
+                    }
+                }
+                shouldShowRequestPermissionRationale(permission) -> showDialog(permission, name, requestCode)
+
+                else -> {
+                    showDialog(permission, name, requestCode)
+                    activity?.let { ActivityCompat.requestPermissions(it, arrayOf(permission), requestCode) }
+                }
+            }
+    }
+
+
+    private fun showDialog(permission: String, name: String, requestCode: Int) {
+        val builder = context?.let { AlertDialog.Builder(it) }
+
+        builder?.apply {
+            setMessage("Permission to access your $name is required to use this app")
+            setTitle("Permission required")
+            setPositiveButton("OK") {dialog, which ->
+                activity?.let { ActivityCompat.requestPermissions(it, arrayOf(permission), requestCode) }
             }
         }
-
-
-
-        return rootView
+        val dialog = builder?.create()
+        dialog?.show()
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
